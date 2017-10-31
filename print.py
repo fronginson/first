9f7aac8d40135c1eb10af9376167770dc336e003
fronginson <frongison@mail.ru>
2017-10-31 22:50:06 +0300

    All other stuff
    Added all stuff, which I had on home pc

--- print.py
+++ print.py
@@ -1,2 +1,21 @@
-name = input('What is your name?')
-print('Hello, ', name + '!')
+def user():
+    login = input('login: ')
+    pswrd = input('password: ')
+    return login, pswrd
+
+
+
+def logon():
+    x = input('enter your login here: ')
+    y = input('enter your password here: ')
+    return x, y
+
+
+def compare():
+    if logon() == user():
+        print('welcome, ' + login.user)
+    else:
+        print('Wrong login or password')
+
+
+compare()
