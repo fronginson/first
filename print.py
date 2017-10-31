0b26600b3670e74ccb4bc84b23b855a6c90b0a2e
fronginson <fronginson@mail.ru>
2017-10-31 17:55:57 +0300

    Building class added

--- print.py
+++ print.py
@@ -1,2 +1,16 @@
 name = input('What is your name?')
 print('Hello, ', name + '!')
+
+
+class Counter:
+    count = 0
+
+    def __init__(self):
+        self.__class__.count +=1
+
+
+print(Counter.count)
+c = Counter()
+print(c.count)
+d = Counter()
+print(d.count)
